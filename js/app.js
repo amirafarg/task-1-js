cc695de73773018b7f268cdece6bc61a3acc0177
amirafarag <amiraazam301@gmail.com>
2024-03-13 01:14:09 +0200

    edit

--- js/app.js
+++ js/app.js
@@ -1,30 +1,30 @@
-// 1-Storing the color of a chosen product (e.g., "red", "blue", "green")
+// 1-Storing the color of a chosen product (e.g., "red", "blue", "green") -->type of data=string
 
-var ColorOfChosenProduct="red";
-console.log(ColorOfChosenProduct)
+var colorOfChosenProduct="red";
+console.log(colorOfChosenProduct ,typeof 'colorOfChosenProduct');
 
-// 2-Representing if a user has subscribed to a newsletter
+// 2-Representing if a user has subscribed to a newsletter -->type of data=Boolean
 
-var CheckSubscribedUser= "tru";
-console.log(CheckSubscribedUser)
+var checkSubscribedUser= true;
+console.log(checkSubscribedUser ,typeof checkSubscribedUser);
 
 
-//3-Calculating the area of a rectangle based on its width and height
+//3-Calculating the area of a rectangle based on its width and height -->type of data=string
 
-var CalculatingAreaOfRectangle="40cm";
-console.log(CalculatingAreaOfRectangle)
+var calculatingAreaOfRectangle="40cm";
+console.log(calculatingAreaOfRectangle ,typeof 'calculatingAreaOfRectangle');
 
-// 4-Displaying a welcome message to the user
+// 4-Displaying a welcome message to the user type of data=string 
 
-var DisplayingInTheUser="welcome";
-console.log(DisplayingInTheUser)
+var displayingInTheUser="welcome";
+console.log(displayingInTheUser ,typeof 'displayingInTheUser');
 
-// 5-Checking if a password meets certain length requirements (using a number) - Number and String combination
+// 5-Checking if a password meets certain length requirements (using a number) - Number and String combination -->type of data=string
 
-var CheckingPassword="245amira245";
-console.log(CheckingPassword)
+var checkingPassword="245amira245";
+console.log(checkingPassword ,typeof 'checkingPassword');
 
-// 6-Keeping track of the number of lives remaining in a game
+// 6-Keeping track of the number of lives remaining in a game -->type of data=number
 
-var TheNumberOfLivesRemainingInAGame="8"
-console.log(TheNumberOfLivesRemainingInAGame)
+var theNumberOfLivesRemainingInAGame= 8;
+console.log(theNumberOfLivesRemainingInAGame ,typeof theNumberOfLivesRemainingInAGame);
