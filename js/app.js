b6b6cb41351aac75329797a2946dd64c4a8501f4
amirafarag <amiraazam301@gmail.com>
2024-03-12 17:31:14 +0200

    new-task

---
+++ js/app.js
@@ -0,0 +1,30 @@
+// 1-Storing the color of a chosen product (e.g., "red", "blue", "green")
+
+var ColorOfChosenProduct="red";
+console.log(ColorOfChosenProduct)
+
+// 2-Representing if a user has subscribed to a newsletter
+
+var CheckSubscribedUser= "tru";
+console.log(CheckSubscribedUser)
+
+
+//3-Calculating the area of a rectangle based on its width and height
+
+var CalculatingAreaOfRectangle="40cm";
+console.log(CalculatingAreaOfRectangle)
+
+// 4-Displaying a welcome message to the user
+
+var DisplayingInTheUser="welcome";
+console.log(DisplayingInTheUser)
+
+// 5-Checking if a password meets certain length requirements (using a number) - Number and String combination
+
+var CheckingPassword="245amira245";
+console.log(CheckingPassword)
+
+// 6-Keeping track of the number of lives remaining in a game
+
+var TheNumberOfLivesRemainingInAGame="8"
+console.log(TheNumberOfLivesRemainingInAGame)
