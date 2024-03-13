5e4577559b2b8b0d0fcdb1e44caad4194202941c
amirafarag <amiraazam301@gmail.com>
2024-03-13 22:17:40 +0200

    new

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // 1-Storing the color of a chosen product (e.g., "red", "blue", "green") -->type of data=string
 
 var colorOfChosenProduct="red";
-console.log(colorOfChosenProduct ,typeof 'colorOfChosenProduct');
+console.log(colorOfChosenProduct ,typeof colorOfChosenProduct);
 
 // 2-Representing if a user has subscribed to a newsletter -->type of data=Boolean
 
@@ -9,20 +9,20 @@ var checkSubscribedUser= true;
 console.log(checkSubscribedUser ,typeof checkSubscribedUser);
 
 
-//3-Calculating the area of a rectangle based on its width and height -->type of data=string
+//3-Calculating the area of a rectangle based on its width and height -->type of data=number
 
-var calculatingAreaOfRectangle="40cm";
-console.log(calculatingAreaOfRectangle ,typeof 'calculatingAreaOfRectangle');
+var rectangleArea="40";
+console.log(rectangleArea ,typeof rectangleArea);
 
 // 4-Displaying a welcome message to the user type of data=string 
 
 var displayingInTheUser="welcome";
-console.log(displayingInTheUser ,typeof 'displayingInTheUser');
+console.log(displayingInTheUser ,typeof displayingInTheUser);
 
-// 5-Checking if a password meets certain length requirements (using a number) - Number and String combination -->type of data=string
+// 5-Checking if a password meets certain length requirements (using a number) - Number and String combination -->type of data=Boolean
 
-var checkingPassword="245amira245";
-console.log(checkingPassword ,typeof 'checkingPassword');
+var passwordVailed= true;
+console.log(passwordVailed ,typeof passwordVailed);
 
 // 6-Keeping track of the number of lives remaining in a game -->type of data=number
 
